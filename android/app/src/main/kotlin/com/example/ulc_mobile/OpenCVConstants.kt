41625215cf3305f2f2a9cce026aaf3b2fe9f781c
Đức Đậu <dauleduc2@gmail.com>
2023-12-08 16:04:58 +0700

    update constants on android

--- android/app/src/main/kotlin/com/example/ulc_mobile/OpenCVConstants.kt
+++ android/app/src/main/kotlin/com/example/ulc_mobile/OpenCVConstants.kt
@@ -3,9 +3,4 @@ package com.example.ulc_mobile
 object OpenCVConstants {
     const val channel : String = "opencv"
     const val process : String = "process"
-    const val ctvColor: String = "ctvColor"
-    const val gaussianBlur: String = "gaussianBlur"
-    const val canny: String = "canny"
-    const val threshold : String = "threshold"
-    const val findContours : String = "findContours"
 }
